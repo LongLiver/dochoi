2ae8d1018f97dc7e7f421af24a5ba3b2e3f58078
LongLiver <105089824+LongLiver@users.noreply.github.com>
2022-06-11 11:40:27 +0700

    abc

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php 
+$servername = "3.132.234.157";
+$username = "longnd";
+$password = "long123";
+$database = "salah";
+//Kết nối theo Mysqli procedural
+$connect = mysqli_connect($servername,$username,$password,$database);
+if($connect){
+	echo "Kết nối thành công";
+}
+else{
+	echo"Kết nối thất bại";
+}
+$result = mysqli_query($connect,$sql);
+if($result){
+	echo"add account success";
+}
+else{
+	echo"add account fail";
+}
+?>
